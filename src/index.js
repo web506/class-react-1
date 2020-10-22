3d5f4d8024d8496f678f1bcd5538bfe23196a14a
web506 <web506@gmail.com>
2020-10-22 11:20:07 -0600

    Class Methods with Constructor

--- src/index.js
+++ src/index.js
@@ -5,22 +5,50 @@ class TarjetaFruta extends React.Component {
 
   constructor () {
     super()
+
+    const METHODS = [
+      'agregar',
+      'quitar',
+      'limpiar'
+    ]
+
+    METHODS.forEach((method) => {
+      this[method] = this[method].bind(this)
+    }
+    )
+
+
     this.state = {
       cantidad: 0
     }
   }
+
+  agregar () {
+    this.setState ({ 
+      cantidad: this.state.cantidad + 1
+    })
+  }
+
+  quitar () {
+    this.setState ({ 
+      cantidad: this.state.cantidad - 1
+    })
+  }
+
+  limpiar () {
+    this.setState ({ 
+      cantidad: 0
+    })
+  }
+
   render () {
     return ( 
       <div>
         <h3>{this.props.name}</h3>
         <div>Cantidad: { this.state.cantidad }</div>
-        <button 
-        onClick={() => {
-          this.setState ({ cantidad: this.state.cantidad + 1})
-        }}
-        >
-          Agregar
-        </button>
+        <button  onClick={this.agregar} > + </button>
+        <button  onClick={this.quitar} > - </button>
+        <button  onClick={this.limpiar} > Limpiar </button>
         <hr/>
         <p>$ {this.props.price}</p>
       </div>
