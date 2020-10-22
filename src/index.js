bdb8434f7dd697306a8aeab2a6f23ad52aec10b7
web506 <web506@gmail.com>
2020-10-22 10:51:24 -0600

    React Component using Classes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+class TarjetaFruta extends React.Component {
+
+  constructor () {
+    super()
+    this.state = {
+      cantidad: 0
+    }
+  }
+  render () {
+    return ( 
+      <div>
+        <h3>{this.props.name}</h3>
+        <div>Cantidad: { this.state.cantidad }</div>
+        <button 
+        onClick={() => {
+          this.setState ({ cantidad: this.state.cantidad + 1})
+        }}
+        >
+          Agregar
+        </button>
+        <hr/>
+        <p>$ {this.props.price}</p>
+      </div>
+    )
+  }
+}
+
+
+
+const App = () => (
+  <div>
+    <TarjetaFruta name='Sandia' price={3.00}/>
+    <TarjetaFruta name='Banano'price={1.00}/>
+    <TarjetaFruta name='Kiwi' price={2.00}/>
+  </div>
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <App />, document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
