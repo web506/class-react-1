68d035c34db5d2798afc9a123f7083ce73b810f7
web506 <web506@gmail.com>
2020-10-22 11:52:01 -0600

    Add Styles to React

--- src/components/TarjetaFruta.js
+++ src/components/TarjetaFruta.js
@@ -1,39 +1,49 @@
-import React from 'react';
+import React from "react";
 
 class TarjetaFruta extends React.Component {
+  state = {
+    cantidad: 0
+  };
 
-    state = {
-       cantidad: 0
-     }
-   
-     agregar = () => this.setState ({ 
-         cantidad: this.state.cantidad + 1
-       })
-   
-   
-     quitar = () =>  this.setState ({ 
-         cantidad: this.state.cantidad - 1
-       })
-   
-   
-     limpiar = () =>  this.setState ({ 
-         cantidad: 0
-       })
-   
-   
-     render () {
-       return ( 
-         <div>
-           <h3>{this.props.name}</h3>
-           <div>Cantidad: { this.state.cantidad }</div>
-           <button  onClick={this.agregar} > + </button>
-           <button  onClick={this.quitar} > - </button>
-           <button  onClick={this.limpiar} > Limpiar </button>
-           <hr/>
-           <p>$ {this.props.price}</p>
-         </div>
-       )
-     }
-   }
-   
-export default TarjetaFruta
+  agregar = () =>
+    this.setState({
+      cantidad: this.state.cantidad + 1
+    });
+
+  quitar = () =>
+    this.setState({
+      cantidad: this.state.cantidad - 1
+    });
+
+  limpiar = () =>
+    this.setState({
+      cantidad: 0
+    });
+
+  render() {
+    const hasItems = this.state.cantidad > 0
+    const styles = {
+        border: '1px solid black',
+        marginBottom: '1em',
+        borderRadius: '0.5em',
+        padding: '1em',
+        background: hasItems ? '#ebebeb' : '#fff',
+        color: hasItems ? '#000' : '#000',
+        transition: 'all 250ms ease-out'
+    }
+    return (
+      <div style={styles}>
+        <h3>{this.props.name}</h3>
+        <div>Cantidad: {this.state.cantidad}</div><br />
+        <button onClick={this.agregar}> + </button>
+        <button onClick={this.quitar}> - </button>
+        <button onClick={this.limpiar}> Limpiar </button>
+        <hr />
+        <p>$ {this.props.price}</p>
+        <p>Total: $ { this.props.price * this.state.cantidad }</p>
+      </div>
+    );
+  }
+}
+
+export default TarjetaFruta;
